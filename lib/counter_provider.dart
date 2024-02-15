09fc78fda281551ff05d67e5f83d5a6815504054
Varinda Khanna <varinda.khanna@adw.kochartech.com>
2024-02-15 13:44:49 +0530

    learnt about state management

---
+++ lib/counter_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CounterProvider extends StateNotifier<int> {
+  CounterProvider() : super(0);
+
+  void increment() => state++;
+  void decrement() => state--;
+}
+
+final counterProvider =
+    StateNotifierProvider<CounterProvider, int>((ref) => CounterProvider());
--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,56 @@
+import 'package:counterapp/counter_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ProviderScope(child: CounterScreen()));
 }
 
-class MyApp extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final _counter = ref.watch(counterProvider);
     return MaterialApp(
-      title: 'Counter App',
-      home: CounterScreen(),
-    );
-  }
-}
-
-class CounterScreen extends StatefulWidget {
-  @override
-  _CounterScreenState createState() => _CounterScreenState();
-}
-
-class _CounterScreenState extends State<CounterScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 5,
-        title: Text('Counter App'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Counter:',
-              style: TextStyle(fontSize: 20),
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 5,
+          title: Text('Counter App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Counter:',
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                '$_counter',
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                ref.read(counterProvider.notifier).decrement();
+              },
+              tooltip: 'Decrement',
+              child: Icon(Icons.remove),
             ),
-            Text(
-              '$_counter',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            SizedBox(width: 10),
+            FloatingActionButton(
+              onPressed: () {
+                ref.read(counterProvider.notifier).increment();
+              },
+              tooltip: 'Increment',
+              child: Icon(Icons.add),
             ),
           ],
         ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: _decrementCounter,
-            tooltip: 'Decrement',
-            child: Icon(Icons.remove),
-          ),
-          SizedBox(width: 10),
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-        ],
       ),
     );
   }
